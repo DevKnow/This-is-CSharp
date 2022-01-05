5ae49fd1b0b03dbb768db025fd433945e13cac8b
DevKnow <helio524@naver.com>
2022-01-05 23:50:21 +0900

    11장_UsingGenericList_420page
    11장_UsingGenericList_420page 예제 따라하기

---
+++ 11장_UsingGenericList_420page/11장_UsingGenericList_420page/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _11장_UsingGenericList_420page
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            for(int i=0; i< 5; i++)
+            {
+                list.Add(i);
+            }
+
+            foreach(int element in list)
+            {
+                Console.Write($"{element} ");
+            }
+            Console.WriteLine();
+
+            list.Remove(2);
+
+            foreach (int element in list)
+            {
+                Console.Write($"{element} ");
+            }
+            Console.WriteLine();
+
+            list.Insert(2, 2);
+
+            foreach (int element in list)
+            {
+                Console.Write($"{element} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
