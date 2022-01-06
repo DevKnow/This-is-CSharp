45629412ca1c150dc9e101f8102b2af7160f7443
DevKnow <helio524@naver.com>
2022-01-06 16:50:50 +0900

    12장_KillingProgram_433page
    12장_KillingProgram_433page 예제 따라하기(일부러 오류가 나도록 프로그래밍함)

---
+++ 12장_KillingProgram_433page/12장_KillingProgram_433page/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _12장_KillingProgram_433page
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3 };
+
+            for(int i=0; i<5; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            Console.WriteLine("종료");
+        }
+    }
+}
