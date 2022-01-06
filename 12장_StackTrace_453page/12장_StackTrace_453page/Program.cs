9eed2c6547a7c0c95c52eeb558e793a301a4fdbe
DevKnow <helio524@naver.com>
2022-01-06 20:50:57 +0900

    12장_StackTrace_453page
    12장_StackTrace_453page 예제 따라하기

---
+++ 12장_StackTrace_453page/12장_StackTrace_453page/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _12장_StackTrace_453page
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = 1;
+                Console.WriteLine(3 / --a);
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+    }
+}
