0eb8d8286feb5da0ef991898975aa18d4473e1f3
DevKnow <helio524@naver.com>
2022-01-06 21:00:58 +0900

    연습문제1_454page
    연습문제1_454page 예제 따라하기

---
+++ 연습문제1_454page/연습문제1_454page/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace 연습문제1_454page
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                    arr[i] = i;
+                for (int i = 0; i < 11; i++)
+                    Console.WriteLine(arr[i]);
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
