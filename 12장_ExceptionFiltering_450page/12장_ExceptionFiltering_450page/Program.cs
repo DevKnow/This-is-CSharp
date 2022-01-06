a2732b87c68373fa2aec5bb4258748a8c67d4afc
DevKnow <helio524@naver.com>
2022-01-06 20:47:30 +0900

    12장_ExceptionFiltering_450page
    12장_ExceptionFiltering_450page 예제 따라하기

---
+++ 12장_ExceptionFiltering_450page/12장_ExceptionFiltering_450page/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _12장_ExceptionFiltering_450page
+{
+    class FilterableException : Exception
+    {
+        public int ErrorNo { get; set; }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter nUMBER bETWEEN 0~10: ");
+            string input = Console.ReadLine();
+
+            try
+            {
+                int num = Int32.Parse(input);
+
+                if (num < 0 || num > 10)
+                    throw new FilterableException() { ErrorNo = num };
+                else
+                    Console.WriteLine($"Output: {num}");
+            }
+            catch (FilterableException e) when(e.ErrorNo<0)
+            {
+                Console.WriteLine("Negative input is not allowed");
+            }
+            catch (FilterableException e) when (e.ErrorNo > 10)
+            {
+                Console.WriteLine("Too big number is not allowed");
+            }
+        }
+    }
+}
