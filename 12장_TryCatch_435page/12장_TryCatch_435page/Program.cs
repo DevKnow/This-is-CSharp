77ecb0645ba991b018cd069911b02da321de2e07
DevKnow <helio524@naver.com>
2022-01-06 17:01:34 +0900

    12장_TryCatch_435page
    12장_TryCatch_435page 예제 따라하기

---
+++ 12장_TryCatch_435page/12장_TryCatch_435page/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _12장_TryCatch_435page
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3 };
+
+            try
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine($"예외 발생: {e.Message}");
+            }
+            
+            Console.WriteLine("종료");
+        }
+    }
+}
