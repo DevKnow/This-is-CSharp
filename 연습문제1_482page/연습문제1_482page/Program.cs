58905d78fc323b4394ab3e14cde9f9b1aa13b2dc
DevKnow <helio524@naver.com>
2022-01-07 21:28:36 +0900

    연습문제1_482page
    연습문제1_482page

---
+++ 연습문제1_482page/연습문제1_482page/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace 연습문제1_482page
+{
+    delegate int MyDelegate(int a, int b);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate Callback;
+
+            Callback = delegate (int a, int b)
+            {
+                return a + b;
+            };
+
+            Console.WriteLine(Callback(3, 4));
+
+            Callback = delegate (int a, int b)
+            {
+                return a - b;
+            };
+
+            Console.WriteLine(Callback(7, 5));
+        }
+    }
+}
