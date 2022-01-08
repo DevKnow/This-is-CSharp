da910345d75078522b63d6156aae6b7fedab760b
DevKnow <helio524@naver.com>
2022-01-08 16:41:57 +0900

    14장_StateLambda_491page
    14장_StateLambda_491page 예제 따라하기

---
+++ 14장_StateLambda_491page/14장_StateLambda_491page/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _14장_StateLambda_491page
+{
+    class Program
+    {
+        delegate string Concantenate(string[] args);
+
+        static void Main(string[] args)
+        {
+            Concantenate concat = (arr) =>
+             {
+                 string result = "";
+                 foreach (string s in arr)
+                     result += s;
+                 return result;
+             };
+
+            Console.WriteLine(concat(args));
+        }
+    }
+}
