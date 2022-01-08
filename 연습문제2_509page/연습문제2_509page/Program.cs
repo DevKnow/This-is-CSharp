4886f148b87215b925fa36b175195af6025b4e61
DevKnow <helio524@naver.com>
2022-01-08 19:00:42 +0900

    연습문제2_509page
    연습문제2_509page

---
+++ 연습문제2_509page/연습문제2_509page/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace 연습문제2_509page
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 11, 22, 33, 44, 55 };
+
+            foreach(int a in array)
+            {
+                Action action = () => Console.WriteLine(a * a);
+                action.Invoke();
+            }
+        }
+    }
+}
