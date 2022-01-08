c4bec5cccd38a2ee8c55ce80de6970d9b26433f2
DevKnow <helio524@naver.com>
2022-01-08 16:30:22 +0900

    14장_SimpleLambda_489page
    14장_SimpleLambda_489page 예제 따라하기

---
+++ 14장_SimpleLambda_489page/14장_SimpleLambda_489page/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _14장_SimpleLambda_489page
+{
+    class Program
+    {
+        delegate int Calculate(int a, int b);
+        static void Main(string[] args)
+        {
+            Calculate cal = (a, b) => a + b;
+
+            Console.WriteLine($"{3} + {4} = {cal(3, 4)}");
+        }
+    }
+}
