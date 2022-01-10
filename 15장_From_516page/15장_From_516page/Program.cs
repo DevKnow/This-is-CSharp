402d208327dd12a88d2216362e4e1d9c0ca0240c
DevKnow <helio524@naver.com>
2022-01-10 18:12:39 +0900

    15장_From_516page
    15장_From_516page

---
+++ 15장_From_516page/15장_From_516page/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _15장_From_516page
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 9, 2, 6, 4, 5, 3, 7, 8, 1, 10 };
+
+            var result = from n in numbers
+                         where n % 2 == 0
+                         orderby n
+                         select n;
+
+            Console.Write("numbers 배열에서 짝수는: ");
+
+            foreach (int n in result)
+                Console.Write($"{n} ");
+        }
+    }
+}
