8d03e7fe29e07302f9174ade596d1c73d5c3f8fd
DevKnow <helio524@naver.com>
2022-01-13 20:39:42 +0900

    17장_DuckTyping_585page
    17장_DuckTyping_585page 예제 따라하기

---
+++ 17장_DuckTyping_585page/17장_DuckTyping_585page/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace _17장_DuckTyping_585page
+{
+    class Duck
+    {
+        public void Walk()
+        {
+            Console.WriteLine(this.GetType() + ".Walk()");
+        }
+
+        public void Swim()
+        {
+            Console.WriteLine(this.GetType() + ".Swim()");
+        }
+
+        public void Quack()
+        {
+            Console.WriteLine(this.GetType() + ".Quack()");
+        }
+    }
+
+    class Robot
+    {
+        public void Walk()
+        {
+            Console.WriteLine(this.GetType() + ".Walk()");
+        }
+
+        public void Swim()
+        {
+            Console.WriteLine(this.GetType() + ".Swim()");
+        }
+
+        public void Quack()
+        {
+            Console.WriteLine(this.GetType() + ".Quack()");
+        }
+    }
+
+    class Mallard : Duck { }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic[] arr = new dynamic[] { new Duck(), new Mallard(), new Robot() };
+
+            foreach(dynamic duck in arr)
+            {
+                Console.WriteLine(duck.GetType());
+                duck.Walk();
+                duck.Swim();
+                duck.Quack();
+                Console.WriteLine();
+            }
+        }
+    }
+}
