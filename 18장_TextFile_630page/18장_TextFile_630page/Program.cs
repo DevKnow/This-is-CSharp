6148c5f5c32590776472a462c1df3b34660f5853
DevKnow <helio524@naver.com>
2022-01-14 22:30:24 +0900

    18장_TextFile_630page
    18장_TextFile_630page 예제 따라하기

---
+++ 18장_TextFile_630page/18장_TextFile_630page/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace _18장_TextFile_630page
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamWriter sw = new StreamWriter(new FileStream("a.txt", FileMode.Create)))
+            {
+                sw.WriteLine(int.MaxValue);
+                sw.WriteLine("Good Morning!");
+                sw.WriteLine(uint.MaxValue);
+                sw.WriteLine("안녕하세요!");
+                sw.WriteLine(double.MaxValue);
+            }
+
+            using (StreamReader sr = new StreamReader(new FileStream("a.txt", FileMode.Open)))
+            {
+                Console.WriteLine($"File Size: {sr.BaseStream.Length} bytes");
+
+                while (sr.EndOfStream == false)
+                {
+                    Console.WriteLine(sr.ReadLine());
+                }
+            }
+
+
+        }
+    }
+}
