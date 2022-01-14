91581c37561f98a32618f236324b9cb41561b831
DevKnow <helio524@naver.com>
2022-01-14 21:51:58 +0900

    Vitamin18_1_621page
    Vitamin18_1_621page 예제 따라하기

---
+++ Vitamin18-1_621page/Vitamin18-1_621page/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace Vitamin18_1_621page
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stream inStream = new FileStream("a.dat", FileMode.Open);
+            for(int i=0; i<3; i++)
+                Console.Write($"{inStream.ReadByte():00} ");
+            inStream.Seek(5, SeekOrigin.Current);
+            Console.Write($"{inStream.ReadByte():00} ");
+        }
+    }
+}
