58c15e58d65009cb833c80a09c714d4dda800bb5
DevKnow <helio524@naver.com>
2022-01-16 10:54:14 +0900

    20장_FormEvent_714page
    20장_FormEvent_714page 예제 따라하기

---
+++ 20장_FormEvent_714page/20장_FormEvent_714page/MainApp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace _20장_FormEvent_714page
+{
+    class MainApp : Form
+    {
+        public void MyMouseHandler(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine($"Sender : {((Form)sender).Text}");
+            Console.WriteLine($"X: {e.X} Y: {e.Y}");
+            Console.WriteLine($"Button: {e.Button} Clicks: {e.Clicks}");
+            Console.WriteLine();
+        }
+
+        public MainApp(string title)
+        {
+            this.Text = title;
+            this.MouseDown += new MouseEventHandler(MyMouseHandler);
+        }
+
+        static void Main(string[] args)
+        {
+            Application.Run(new MainApp("Mouse Evner Test"));
+        }
+    }
+}
