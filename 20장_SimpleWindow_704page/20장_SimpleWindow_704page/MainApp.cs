2f03f59aa4fa5b1be433dfae7e20f8b324aa0def
DevKnow <helio524@naver.com>
2022-01-16 10:04:25 +0900

    20장_SimpleWindow_704page
    20장_SimpleWindow_704page 예제 따라하기

---
+++ 20장_SimpleWindow_704page/20장_SimpleWindow_704page/MainApp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _20장_SimpleWindow_704page
+{
+    class MainApp : System.Windows.Forms.Form
+    {
+        static void Main(string[] args)
+        {
+            System.Windows.Forms.Application.Run(new MainApp());
+        }
+    }
+}
